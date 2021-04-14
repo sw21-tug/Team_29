e5a9957ab74cfee6f6aefd7c68d587a7fcb91a87
Raphael Innig <raphael.innig@student.tugraz.at>
2021-04-14 15:04:12 +0200

    MULI-5 added fab and start new game activity [RI,JB]

--- MulatschakTracker/app/src/androidTest/java/com/example/mulatschaktracker/ExampleInstrumentedTest.kt
+++ MulatschakTracker/app/src/androidTest/java/com/example/mulatschaktracker/ExampleInstrumentedTest.kt
@@ -1,5 +1,10 @@
 package com.example.mulatschaktracker
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,18 +12,39 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+
+
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
+
+
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.mulatschaktracker", appContext.packageName)
+    }
+
+    @Test
+    fun startNewGameActivity() {
+        //Test for the activity of starting a new game
+        onView(withId(R.id.StartNewGameButton)).perform(click())
+
+
+
     }
 }
---
+++ MulatschakTracker/app/src/main/java/com/example/mulatschaktracker/StartNewGame.kt
@@ -0,0 +1,11 @@
+package com.example.mulatschaktracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartNewGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_new_game)
+    }
+}
