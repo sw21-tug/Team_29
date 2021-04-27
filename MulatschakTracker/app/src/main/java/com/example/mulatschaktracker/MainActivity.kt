b3c69342bcc307a76260c50335be3a6f01cb2488
jbreitenthaler <jonathanbreitenthaler@gmail.com>
2021-04-27 16:13:20 +0200

    MULI-5 Merge Fixes [RI,JB]

--- MulatschakTracker/app/src/main/java/com/example/mulatschaktracker/MainActivity.kt
+++ MulatschakTracker/app/src/main/java/com/example/mulatschaktracker/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 
 import android.view.View
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
