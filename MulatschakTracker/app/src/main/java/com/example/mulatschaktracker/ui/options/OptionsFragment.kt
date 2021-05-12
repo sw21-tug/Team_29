7fe9de00b95a8a08bcc654a843a8d5fcf6bfe771
Johannes Leibetseder <johannes.leibetseder@tugraz.at>
2021-05-12 15:49:30 +0200

    MULI-3 Delete my account [BV, LJ] added implementation

--- MulatschakTracker/app/src/main/java/com/example/mulatschaktracker/ui/options/OptionsFragment.kt
+++ MulatschakTracker/app/src/main/java/com/example/mulatschaktracker/ui/options/OptionsFragment.kt
@@ -1,6 +1,8 @@
 package com.example.mulatschaktracker.ui.options
 
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +13,7 @@ import androidx.lifecycle.Observer
 
 import androidx.lifecycle.ViewModelProvider
 import com.example.mulatschaktracker.R
+import com.example.mulatschaktracker.UserRepository
 
 class OptionsFragment : Fragment() {
 
@@ -25,5 +28,22 @@ class OptionsFragment : Fragment() {
                 ViewModelProvider(this).get(OptionsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_options, container, false)
         return root
+    }
+
+    fun onDeleteUserPressed(view: View){
+
+        val context = getActivity()
+        val du = AlertDialog.Builder(context)
+
+        du.setPositiveButton("Yes",  DialogInterface.OnClickListener { arg0, arg1 ->
+            //refresh application screen
+
+            val userRepo = context?.let { UserRepository(it) }
+            if (userRepo != null) {
+                userRepo.resetDatabase()
+            }
+        })
+        du.setTitle(getString(R.string.alert_delete_user))
+        du.show()
     }
 }
