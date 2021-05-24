e281df82bbf424042af7475422f478d077b20382
Tim Celec <timcelec1@gmail.com>
2021-05-24 20:29:00 +0200

    [MULI-8] DataBase update Testcases scalable update 5[CT, IR]

--- MulatschakTracker/app/src/androidTest/java/com/example/mulatschaktracker/GameFinishedTest.kt
+++ MulatschakTracker/app/src/androidTest/java/com/example/mulatschaktracker/GameFinishedTest.kt
@@ -509,4 +509,104 @@ class GameFinishedTest {
     }
 
 
+    @Test
+    fun getWinnerList1234()
+    {
+
+        val appContext: Context = ApplicationProvider.getApplicationContext();
+        val repo = GameRepository(appContext);
+
+        val appContext2: Context = ApplicationProvider.getApplicationContext();
+        val repo2 = UserRepository(appContext2);
+        repo2.resetDatabase()
+        onView(withId(R.id.StartNewGameActivityButton)).perform(click())
+
+
+        onView(withId(R.id.StartNewGameButton)).perform(click())
+
+        onView(withId(R.id.EndGameButton)).perform(ViewActions.click())
+
+        for (i in 0..21) {
+            onView(withId(R.id.button_player_1)).perform(ViewActions.click())
+            onView(withId(R.id.button_player_2)).perform(ViewActions.click())
+            onView(withId(R.id.button_player_3)).perform(ViewActions.click())
+            onView(withId(R.id.button_player_4)).perform(ViewActions.click())
+        }
+        onView(withId(R.id.endround)).perform(click())
+
+        var game =  repo.getGameFinished(1)
+        assert(game == 1)
+        val listOfWinners = repo.getWinners(1)
+        assert(listOfWinners.player1 == "Player 1")
+        assert(listOfWinners.player2 == "Player 2")
+        assert(listOfWinners.player3 == "Player 3")
+        assert(listOfWinners.player4 == "Player 4")
+    }
+
+
+
+    @Test
+    fun getWinnerList13()
+    {
+
+        val appContext: Context = ApplicationProvider.getApplicationContext();
+        val repo = GameRepository(appContext);
+
+        val appContext2: Context = ApplicationProvider.getApplicationContext();
+        val repo2 = UserRepository(appContext2);
+        repo2.resetDatabase()
+        onView(withId(R.id.StartNewGameActivityButton)).perform(click())
+
+
+        onView(withId(R.id.StartNewGameButton)).perform(click())
+
+        onView(withId(R.id.EndGameButton)).perform(ViewActions.click())
+
+        for (i in 0..21) {
+            onView(withId(R.id.button_player_1)).perform(ViewActions.click())
+            onView(withId(R.id.button_player_3)).perform(ViewActions.click())
+        }
+        onView(withId(R.id.endround)).perform(click())
+
+        var game =  repo.getGameFinished(1)
+        assert(game == 1)
+        val listOfWinners = repo.getWinners(1)
+        assert(listOfWinners.player1 == "Player 1")
+        assert(listOfWinners.player2 == "Player 3")
+    }
+
+
+
+
+
+    @Test
+    fun getWinnerList24()
+    {
+
+        val appContext: Context = ApplicationProvider.getApplicationContext();
+        val repo = GameRepository(appContext);
+
+        val appContext2: Context = ApplicationProvider.getApplicationContext();
+        val repo2 = UserRepository(appContext2);
+        repo2.resetDatabase()
+        onView(withId(R.id.StartNewGameActivityButton)).perform(click())
+
+
+        onView(withId(R.id.StartNewGameButton)).perform(click())
+
+        onView(withId(R.id.EndGameButton)).perform(ViewActions.click())
+
+        for (i in 0..21) {
+            onView(withId(R.id.button_player_2)).perform(ViewActions.click())
+            onView(withId(R.id.button_player_4)).perform(ViewActions.click())
+        }
+        onView(withId(R.id.endround)).perform(click())
+
+        var game =  repo.getGameFinished(1)
+        assert(game == 1)
+        val listOfWinners = repo.getWinners(1)
+        assert(listOfWinners.player1 == "Player 2")
+        assert(listOfWinners.player2 == "Player 4")
+    }
+
 }
