0ed97da2f899dd1f6631fa8749bd1aa60fa86c6c
mmeglitsch <mario.meglitsch@student.tugraz.at>
2021-06-02 16:55:05 +0200

    [MULI-27] -Fixed Game Mode [CT,MM]

--- MulatschakTracker/app/src/androidTest/java/com/example/mulatschaktracker/GameRuleTest.kt
+++ MulatschakTracker/app/src/androidTest/java/com/example/mulatschaktracker/GameRuleTest.kt
@@ -1,7 +1,7 @@
 package com.example.mulatschaktracker
 
 import android.content.Context
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
@@ -12,29 +12,12 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import junit.framework.TestCase
 import org.junit.Test
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Rule
+
 import org.junit.runner.RunWith
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+
 import org.junit.After
 import org.junit.Before
-import android.content.Context
-import android.view.View
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.*
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.mulatschaktracker.ui.addGameRound.AddGameRoundActivityTest.Companion.getText
-import junit.framework.TestCase
-import org.hamcrest.Matcher
 
 
 @RunWith(AndroidJUnit4::class)
