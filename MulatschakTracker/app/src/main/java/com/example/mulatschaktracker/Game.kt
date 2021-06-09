b2f4477288b63a5ec9ff8dd286529a08b4b3f858
Raphael Innig <raphael.innig@student.tugraz.at>
2021-06-09 16:42:19 +0200

    [MULI-33] Carinthian Mode not handled correctly in new games [IR,LJ] fixed error in game

--- MulatschakTracker/app/src/main/java/com/example/mulatschaktracker/Game.kt
+++ MulatschakTracker/app/src/main/java/com/example/mulatschaktracker/Game.kt
@@ -48,6 +48,11 @@ class Game : AppCompatActivity() {
 
         val game = repository.getGame(intent.getLongExtra(GAME_ID, 0))
         game.gamemode = repository.getGameMode(intent.getLongExtra(GAME_ID, 0))
+
+        if(game.gamemode == 1)
+        {
+            points = 15;
+        }
         findViewById<TextView>(R.id.textViewPlayer1).apply {
             text = game.player1
         }
@@ -118,7 +123,7 @@ class Game : AppCompatActivity() {
             val newText = TextView(this)
             newText.id = i + 1
             newText.inputType = TYPE_CLASS_NUMBER
-            newText.text = 21.toString()
+            newText.text = points.toString()
             newText.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize)
             newText.gravity = Gravity.CENTER
             newRow.addView(newText, layoutParams)
